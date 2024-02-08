57f49d68d1099e7ffbad872e5a1d0064747a0a8b
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2024-02-09 06:55:38 +0700

    🛠️ fix github page build

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import path from 'path'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [svelte()],
-	resolve: {
-		alias: {
+export default defineConfig(({ command, mode, ssrBuild }) => {
+    if (command === 'serve') {
+        return {
+            plugins: [svelte()],
+            resolve: {
+                alias: {
 			'@config': path.resolve(__dirname, './src/config.ts'),
 			'@components': path.resolve(__dirname, './src/lib/components/index.ts'),
 			'@motion': path.resolve(__dirname, './src/lib/motion/index.ts'),
@@ -14,6 +15,24 @@ export default defineConfig({
 			'@lib': path.resolve(__dirname, './src/lib'),
 			'@stores': path.resolve(__dirname, './src/lib/stores'),
 			'@styles': path.resolve(__dirname, './src/lib/styles'),
-		},
-	},
+                },
+            },
+        }
+    }
+
+    return {
+        plugins: [svelte()],
+        base: '/project-erebus-presentation//',
+        resolve: {
+            alias: {
+			'@config': path.resolve(__dirname, './src/config.ts'),
+			'@components': path.resolve(__dirname, './src/lib/components/index.ts'),
+			'@motion': path.resolve(__dirname, './src/lib/motion/index.ts'),
+			'@languages': path.resolve(__dirname, './src/lib/languages/index.ts'),
+			'@lib': path.resolve(__dirname, './src/lib'),
+			'@stores': path.resolve(__dirname, './src/lib/stores'),
+			'@styles': path.resolve(__dirname, './src/lib/styles'),
+            },
+        },
+    }
 })
